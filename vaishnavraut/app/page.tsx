5e85bd35ad65fc275e3a16967f56bfb9d01818c5
vaishnavrraut <vaishnavrraut@gmail.com>
2026-04-08 05:02:28 +0530

    5

--- vaishnavraut/app/page.tsx
+++ vaishnavraut/app/page.tsx
@@ -1,3 +1,22 @@
+import "./globals.css";
+
+
 export default function Page() {
-  return <h1>Hello</h1>;
+  return (
+     <main>
+      <nav className="navbar">
+        <h1 className="logo">Vaishnav Raut</h1>
+        <ul className="nav-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+       <footer className="footer">
+      <p>&copy; 2024 Vaishnav Raut. All rights reserved.</p>
+     </footer>
+     </main>
+    
+  );
 }
