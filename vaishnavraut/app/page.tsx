7d43e8ad157505961d3df2b4d63425cc0c98059f
vaishnavrraut <vaishnavrraut@gmail.com>
2026-04-12 15:50:45 +0530

    3

--- vaishnavraut/app/page.tsx
+++ vaishnavraut/app/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
         <section id="home" className="home">
   <div className="home-content">
     <div className="image">
-      <img src="vaishnavraut/public/photograph-2.jpg" alt="Vaishnav" />
+      <img src="/photograph-2.jpg" alt="Vaishnav" />
     </div>
     <div className="text">
       <h1>Hi, I'm Vaishnav</h1>
